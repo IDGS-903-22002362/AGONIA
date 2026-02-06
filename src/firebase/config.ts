67d0ce25bc150a5374f1f36123000e7255b25789
Alberto Rosas <luisrosasbocanegra@gmail.com>
2026-02-06 17:17:30 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7418002012-1961f",
+  "appId": "1:1074937359778:web:2d6bad97a21023e36196fb",
+  "apiKey": "AIzaSyAli_-M0UQbc25WL6gZ4YF0OVNpBrrjJpc",
+  "authDomain": "studio-7418002012-1961f.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1074937359778"
+};
